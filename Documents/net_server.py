2078de867af6e1591c2a6a19e10d9140912b7a6f
Zach Kirch <zkirch@Zachs-MacBook-Pro.local>
2019-09-02 08:24:59 -0700

    Server Code

---
+++ Documents/net_server.py
@@ -0,0 +1,26 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+addr = ("127.0.0.1", 12349)
+sock.bind(addr)
+
+sock.listen(5)
+
+print('This is the Server')
+
+while True:
+    (connectedSock, clientAddress) = sock.accept()
+    try:
+        msg = connectedSock.recv(1024).decode()
+    except:
+        connectedSock.close()        
+    message = "The band " + msg + " Suck ASS"
+
+    connectedSock.sendall(message.encode())
+    
+
+
+
